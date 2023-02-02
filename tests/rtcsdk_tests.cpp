20795ee2eae10d779642045a1a91e01b00650b39
wxinix <xinwuping007@gmail.com>
2023-02-01 21:33:26 -0500

    Add doctest test.

--- tests/rtcsdk_tests.cpp
+++ tests/rtcsdk_tests.cpp
@@ -1,10 +1,11 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
 #define RTCSDK_COM_NO_LEAK_DETECTION
 
-#include <iostream>
 #include <rtcsdk/interfaces.h>
-#include <windows.h>
+#include <rtcsdk/vector.h>
 
-// Declare sample interface
 RTCSDK_DEFINE_INTERFACE(ISampleInterface, "{AB9A7AF1-6792-4D0A-83BE-8252A8432B45}")
 {
   [[nodiscard]] virtual int sum(int a, int b) const noexcept = 0;
@@ -15,7 +16,7 @@ RTCSDK_DEFINE_INTERFACE(ISampleInterface, "{AB9A7AF1-6792-4D0A-83BE-8252A8432B45
 class __declspec(novtable) sample_object : public rtcsdk::object<sample_object, ISampleInterface>
 {
 public:
-  explicit sample_object(int default_answer) noexcept : default_answer_{default_answer}
+  explicit sample_object(int default_answer) noexcept: default_answer_{default_answer}
   {
   }
 
@@ -34,11 +35,16 @@ private:
   int default_answer_;
 };
 
-int main()
+TEST_CASE("sample interface works as expected.")
 {
-  // Create new instance and get its ISampleInterface interface pointer
-  {
-    auto obj = sample_object::create_instance(42).to_ptr();
-    std::cout << obj->sum(obj->get_answer(), 5);
+  auto obj = sample_object::create_instance(42).to_ptr();
+  CHECK_EQ(obj->sum(obj->get_answer(), 5), 47);
+}
+
+TEST_CASE("vector type traits")
+{
+  using namespace rtcsdk::details;
+  SUBCASE("remove_front_t works as expected") {
+    CHECK(std::is_same_v<remove_front_t<vector<int, float, double>>, vector<float, double>>);
   }
 }
